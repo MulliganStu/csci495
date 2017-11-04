03f62928a7173c180ccc5ba67d63a17b0e899085
Jonathan Winters <jwintersrok@gmail.com>
2017-11-04 15:39:13 -0400

    WE DONE

---
+++ jenga/Assets/scripts/camControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camControl : MonoBehaviour {
+
+	public GameObject target = null;
+	public bool orbitY = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target != null) {
+			transform.LookAt (target.transform);
+			if (orbitY) {
+				transform.RotateAround (target.transform.position, Vector3.up, Time.deltaTime * 5);
+			}
+		}
+
+	}
+}
